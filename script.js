9ff6dec49f233044b829dfc222ecb7c6e95f9812
Anshu3301 <134282893+Anshu3301@users.noreply.github.com>
2023-12-09 11:54:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+ const Date ="Get Date Function"
+ //var a = new Date();
+
+// function fullDate(){
+//   document.write(a);
+//   alert(typeof a);
+// }
+
+//  year = () => {
+//     var x = a.getFullYear();
+//     document.write(x);
+//     alert(typeof x);
+//   }
+
+//   function month(){
+//     var x = a.getMonth();
+//     var months = ["January","february","march", "april", "may", "june","july","August","Sept","Oct","Nov","Dec"]
+//     document.write(months[x]);
+//     alert(typeof months);
+//   }
+
+//   function date(){
+//     var x = a.getDate();
+//     document.write(x);
+//     alert(typeof x);
+//   }
+
+//   function hour(){
+//     var x = a.getHours();
+//     document.write(x);
+//     alert(typeof x);
+//   }
+
+//   function minute(){
+//     var x = a.getMinutes();
+//     document.write(x);
+//     alert(typeof x);
+//   }
+
+//   function second(){
+//     var x = a.getSeconds();
+//     document.write(x);
+//     alert(typeof x);
+//   }
+
+//   function day(){
+//     var x = a.getDay();
+//     var weekdays = ["Sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+//     document.write(weekdays[x]);
+//     alert(typeof x);
+//   }
+
+// var x;
+// function openWin() {
+//  x = window.open("https://fontawesome.com/");   // , "height=100px", "width=600px");
+// }
+
+// function closeWin() {
+//   x.close();
+//  }
+
+// alert(isNaN("rje"));
+
+
+//setInterval(a,1500);
+// setTimeout(a,2500);
+
+let Toggle = "Toggle Button";
+// let toggle = () => {
+
+// var a = document.getElementById("button").innerHTML;
+
+// if(a == "Show Password"){
+//     var x = document.getElementById('input1');
+//     x.setAttribute("type", "text");
+//     document.getElementById("button").innerText = "Hide Password";
+// }
+// else{
+//   var x = document.getElementById('input1');
+//   x.setAttribute("type", "password");
+//   document.getElementById("button").innerText  = "Show Password";
+// }
+// }
+
+// function clear(){
+//   var z = document.getElementById('input2').innerText;    // Why not working??
+//   z = " ";
+// }
+
+let z=0;
+// var x = ['Anshu','b','c','d'];
+
+// x.forEach(add);
+
+// function add(value,index,array){
+//   array[index] = value + ' 10';
+// }
+
+// x.forEach(after);
+
+// function after(value){
+// document.write(value +'<br>');
+// };
+
+import {sum} from "./export.js"
+//alert(sum(9,1));
+
+
+// document.getElementById("demo1").innerHTML=
+let x= document.createElement("Button");
+x.innerHTML='Click Me!';
+let a= document.querySelector("div");
+a.append(x);
